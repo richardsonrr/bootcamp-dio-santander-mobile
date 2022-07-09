b27c8138f4274587a19056d295f544718ad248fb
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-07-09 20:03:01 -0300

    refactoring

--- ProjetoPlacar/app/src/main/java/com/projetoplacar/dev/richardsonrodrigues/ui/DetailActivity.kt
+++ ProjetoPlacar/app/src/main/java/com/projetoplacar/dev/richardsonrodrigues/ui/DetailActivity.kt
@@ -1,5 +1,6 @@
-package com.projetoplacar.dev.richardsonrodrigues
+package com.projetoplacar.dev.richardsonrodrigues.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.projetoplacar.dev.richardsonrodrigues.databinding.ActivityDetailBinding
@@ -13,6 +14,25 @@ class DetailActivity : AppCompatActivity() {
 
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val toolbar = binding.toolbar
+
+        setSupportActionBar(toolbar)
+         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.toolbar.
+        setOnClickListener{
+            val mainActivity = Intent(this, MainActivity::class.java)
+            startActivity(mainActivity)
+            binding.toolbar.title = "Richardson"
+
+
+        }
+
+
+
+
+
 
 
     }
---
+++ ProjetoPlacar/app/src/main/java/com/projetoplacar/dev/richardsonrodrigues/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+ package com.projetoplacar.dev.richardsonrodrigues.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.projetoplacar.dev.richardsonrodrigues.databinding.ActivityMainBinding
+
+ class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
