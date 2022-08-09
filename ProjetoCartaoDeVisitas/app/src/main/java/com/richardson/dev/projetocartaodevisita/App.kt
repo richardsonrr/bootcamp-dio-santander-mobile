5d2679ec95a4db59cdd55d786aef2fcb7ce370ef
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-08-08 23:17:35 -0300

    project init and designing main layout

---
+++ ProjetoCartaoDeVisitas/app/src/main/java/com/richardson/dev/projetocartaodevisita/App.kt
@@ -0,0 +1,7 @@
+package com.richardson.dev.projetocartaodevisita
+
+import android.app.Application
+
+class App:Application() {
+
+}
---
+++ ProjetoCartaoDeVisitas/app/src/main/java/com/richardson/dev/projetocartaodevisita/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.richardson.dev.projetocartaodevisita.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.richardson.dev.projetocartaodevisita.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
