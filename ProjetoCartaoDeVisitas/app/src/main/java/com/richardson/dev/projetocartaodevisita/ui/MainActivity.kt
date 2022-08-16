5d20c21c72325a7133b84729151818cd271a65ac
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-08-15 21:15:27 -0300

    add onClick fab add new card.

--- ProjetoCartaoDeVisitas/app/src/main/java/com/richardson/dev/projetocartaodevisita/ui/MainActivity.kt
+++ ProjetoCartaoDeVisitas/app/src/main/java/com/richardson/dev/projetocartaodevisita/ui/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.richardson.dev.projetocartaodevisita.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.richardson.dev.projetocartaodevisita.R
+import androidx.appcompat.app.AppCompatActivity
+import com.richardson.dev.projetocartaodevisita.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        insetListener()
+
+    }
+
+    private fun insetListener(){
+        binding.fabAdd.setOnClickListener {
+            val intent = Intent(this,AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
